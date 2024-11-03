940714cc88ec4ce5314f9f254c491c938013ead9
Jed Aficial's PC <jedaficial1@gmail.com>
2024-11-03 15:58:06 -0800

    10-minute update

--- 210-demo-std-map.cpp
+++ 210-demo-std-map.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 //
 int villager_menu();
+void villager_output();
 
 int villager_menu(){
     int choice;
@@ -15,12 +16,31 @@ int villager_menu(){
     cout << setw(10) << "" << "4. Decrease Friendship" << endl;
     cout << setw(10) << "" << "5. Search for Villager" << endl;
     cout << setw(10) << "" << "6. Exit" << endl;
+    cout << setw(10) << "" << "Enter choice:";
     cin >> choice;
 }
+
+void villager_output(map<string, tuple<string, int, string, string>> &villager_map){
+
+}
 int main() {
-    // declarations
+    
+    int menu_choice;
+    bool menu_flag = true;
     map<string, tuple<string, int, string, string>> villager; // key and value pair right here
 
+    while (menu_flag){
+        menu_choice = villager_menu();
+        if (menu_choice == 6){ // if the user decides to exit the program
+            menu_flag = false;
+        }
+        
+        else if (menu_choice == 1){
+
+        }
+        
+
+    }
     /*NOTE: Need to convert the vector into a tuple.
             Also changing the parameters of the villager: 
                 - string villager_name, int friendship_points(0-10), 
